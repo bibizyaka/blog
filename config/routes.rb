059d2b6382b1a368347a0dc74aaff2cd54005f28
gali <gbmatrix@gmail.com>
2018-12-26 16:16:40 +0200

    add contacts controller using source

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,19 @@ Rails.application.routes.draw do
  # get 'home/index'
 get '/' => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :articles
 
-end
+resource :contacts, only: [:new, :create]
+
+resources :articles
+#resources :contacts
+
+
+#get 'contacts/new'
+#get 'contacts' => 'contacts#new'
+
+#post 'contacts/create'
+
+end #rails do
+
+
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_25_203756) do
+ActiveRecord::Schema.define(version: 2018_12_25_223939) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
