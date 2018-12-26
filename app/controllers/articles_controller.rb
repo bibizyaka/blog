ae6e2ecb96a7e2e44d4100e1b949ec30d7b08156
gali <gbmatrix@gmail.com>
2018-12-27 00:50:43 +0200

    articles create method added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,19 @@ class ArticlesController < ApplicationController
    end
 
    def create
-   
-     render plain: params[:article]
+      #render plain: params[:article]
+      @article = Article.new(article_params)
+      if @article.save
+          #nothing...cause already saved in if statement
+      else
+           render action: 'new'
+      end
+    end #create
 
-   end 
-end
+    private
+
+       def article_params
+
+         params.require(:article).permit(:title, :text)          
+       end
+end #class
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,19 @@ get '/' => 'home#index'
 #get 'contacts' => 'contacts#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+#contacts
 resource :contacts, only: [:new, :create], path_names: { :new => '' }
 #resource :contacts, only: [:create]
-resources :articles
+
+#articles
+#get 'articles' => 'articles#new'
+resource :articles, only: [:new, :create], path_names: { :new => '' }
+
+#about
+resources :about
+
+#terms
+resources :terms
 
 end #rails do
 
