392b178217364429454e2d75949e28c58fb4fd8b
gali <gbmatrix@gmail.com>
2018-12-29 17:40:20 +0200

    implement article show and edit/update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,15 @@
 class ArticlesController < ApplicationController
+   
+   def index
+   
+       @article = Article.all
+      
+   end
+
+   def show 
+      @article = Article.find(params[:id])
+   end
+
    def new
    end
 
@@ -6,11 +17,26 @@ class ArticlesController < ApplicationController
       #render plain: params[:article]
       @article = Article.new(article_params)
       if @article.save
+        redirect_to @article
           #nothing...cause already saved in if statement
       else
            render action: 'new'
       end
     end #create
+   
+   def edit # for loading the record in view for update
+      @article = Article.find(params[:id])
+   end #edit
+
+   def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+         redirect_to @article
+      else
+          render action: 'edit'
+      end
+
+   end# update
 
     private
 
@@ -18,4 +44,7 @@ class ArticlesController < ApplicationController
 
          params.require(:article).permit(:title, :text)          
        end
+
+
+
 end #class
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  # get 'home/index'
 get '/' => 'home#index'
+#get '/' => 'article#index'
 #get 'contacts' => 'contacts#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -9,8 +10,10 @@ resource :contacts, only: [:new, :create], path_names: { :new => '' }
 #resource :contacts, only: [:create]
 
 #articles
+get 'articles/new' => 'articles#new'
+get 'articles' => 'articles#index'
 #get 'articles' => 'articles#new'
-resource :articles, only: [:new, :create], path_names: { :new => '' }
+resources :articles, only: [:new, :create, :show, :edit, :update], path_names: { :new => '' }
 
 #about
 resources :about
