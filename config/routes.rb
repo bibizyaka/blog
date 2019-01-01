f0aced4d675dacf4caca1e79934e0e185e3d1621
gali <gbmatrix@gmail.com>
2019-01-01 19:30:30 +0200

    add device gem and user eshut

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
  
+devise_for :users
+
  # get 'home/index'
 get '/' => 'home#index'
-#get '/' => 'article#index'
-#get 'contacts' => 'contacts#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root to: "home#index"
 
 #contacts
 resource :contacts, only: [:new, :create] #, path_names: { :new => '' }
