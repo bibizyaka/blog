c919816d609a43fd06f84bb4c416e290d5767e86
gali <gbmatrix@gmail.com>
2019-01-02 18:25:02 +0200

    added username field and showing it on sign in on top

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
 
       def configure_permitted_parameters
     
-        # devise_parameter_sanitizer.for(:sign_up) << :username
          added_attrs = [:username]
          devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
       end
