fd2c56abf9e27624583de900ef58fa669c07b561
gali <gbmatrix@gmail.com>
2019-01-08 17:41:00 +0200

    update test to use with I18n.t

--- spec/feature/user_create_contact_spec.rb
+++ spec/feature/user_create_contact_spec.rb
@@ -4,7 +4,7 @@ feature "Contact Creation" do
    scenario "allows access to contact page" do
      visit '/contacts'
 
-     expect(page).to have_content 'Contact Us' # case sensetive 
+     expect(page).to have_content I18n.t('contacts.contact_us') #Case sensetive 
                                                # (in order to not have sensetivity we will
                                                # use i18n... )
    end #scenario
