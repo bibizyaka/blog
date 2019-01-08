307907f2879255abd974c19175b470410500ac14
gali <gbmatrix@gmail.com>
2019-01-08 17:28:17 +0200

    update contacts new view using i18n

--- spec/feature/user_create_contact_spec.rb
+++ spec/feature/user_create_contact_spec.rb
@@ -4,8 +4,9 @@ feature "Contact Creation" do
    scenario "allows access to contact page" do
      visit '/contacts'
 
-     expect(page).to have_content 'Contact Us' 
+     expect(page).to have_content 'Contact Us' # case sensetive 
+                                               # (in order to not have sensetivity we will
+                                               # use i18n... )
    end #scenario
  
-
 end #feature
